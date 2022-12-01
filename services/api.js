94567ea178577ba979d1e059c133cd16ffb247c0
Luciano Rafael da Rosa <95109800+Luciano-1983@users.noreply.github.com>
2022-12-01 20:07:59 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -3,10 +3,9 @@ const axios = require('axios').default;
 
 // Pode ser algum servidor executando localmente: 
 //baseURL: "https://cuidadorb.azurewebsites.net",
-// http://localhost:3000
+//http://localhost:3000
 
-const api = axios.create({
-  
+const api = axios.create({ 
   baseURL: "https://cuidadorb.azurewebsites.net",
 });
 
